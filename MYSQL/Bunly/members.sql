8e8dff51a9edeaea5e7c1ee91a0671633f4319d7
sembunly <sembunly2005@gmail.com>
2026-07-05 01:46:57 +0700

    Ignore .DS_Store

--- MYSQL/Bunly/members.sql
+++ MYSQL/Bunly/members.sql
@@ -18,7 +18,7 @@ CREATE TABLE members (
     PRIMARY KEY (id)
 ) ENGINE=InnoDB
 DEFAULT CHARSET=utf8mb4
-COLLATE=utf8mb4_0900_ai_ci;
+COLLATE=utf8mb4_0900_ai_ci;5
 
 -- ==========================================
 -- Insert Sample Data
